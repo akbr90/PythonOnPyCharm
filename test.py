725016d7ef26c2772cb177f052bfa8b8e142a29f
akbr90 <akbr90@gmail.com>
2022-02-23 22:23:05 +0530

    First commit for pycharm

---
+++ test.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = 30 if a > b else 40
+print(c)
+
+print(type(a))
+print(type(2.5))
+print(type(True))
+print(type('a'))
+print(type("Akbar"))
+print(type([2]))
+print(type({2}))
+print((type((2,))))
+print(type({2: "A"}))
+
